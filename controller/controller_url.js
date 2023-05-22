e313f65703053bafb297a20f84887cba3838726f
Yousuf-Mohammad <yousuf.mohammad8783@gmail.com>
2023-05-22 07:31:50 +0600

    final check

--- controller/controller_url.js
+++ controller/controller_url.js
@@ -1,5 +1,6 @@
+
+const URL = require("../model/model_url");
 const shortid = require('shortid');
-const URL = require("../model/model_url")
 
 
 
@@ -8,14 +9,14 @@ async function handleGenerateShortURL(req, res) {
     const body = req.body;
     if (!body.url) return res.status(400).json({ error: "url is required" });
 
-    const shortid = shortid();
+    const shortId = shortid();
 
     await URL.create({
-        shortId: shortID,
+        shortId: shortId,
         redirectURL: body.url,
         visitHistory: [],
     });
-    return res.json({ id: shortID });
+    return res.json({ id: shortId });
 }
 
 
--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require("express");
 const urlRoute = require("./routes/routes_url");
 const URL = require("./model/model_url");
+const connectToMongoDB = require("./connect")
 
 
 const app = express(); // initiating express app 
-
-
 const PORT = 8000;
+
+connectToMongoDB("mongodb+srv://dbUser:Zq4UcF8pwSq1tQiw@cluster0.h3ajy.mongodb.net/micro-url").then(() => {
+    console.log("MongoDB Connected")
+});
+
+app.use(express.json());
+app.use("/url", urlRoute);
+
+app.get("/:shortId", async (req, res) => {
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate(
+        {
+            shortId,
+        },
+        {
+            $push: {
+                visitHistory: {
+                    timestamp: Date.now(),
+                },
+            },
+        }
+    );
+    res.redirect(entry.redirectURL);
+});
+
+app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`))
